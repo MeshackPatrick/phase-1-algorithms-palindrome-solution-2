a5b594beb86292c1b4052c6cf3abd62ef22b5cc3
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-06-30 13:05:15 +0300

    palidrome 2

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //If the first letter is the same as the last letter, and the second letter is the same as the second to last letter, and so on (until the middle of the word), the function should return true.
 }
 
 /* 
   Add your pseudocode here
 */
+//iterate from the beginning of the string to the middle of the string
+ // compare the letter we're iterating over to the corresponding letter at the end of the string
+   // if the letters don't match, return false
+
+//giif we reach the middle, and all the letters match, return true
 
 /*
   Add written explanation of your solution here
